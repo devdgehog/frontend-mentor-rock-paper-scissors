a648ecbc2c47d631aee9da0397bd33ea00f857f9
devdgehog <72769867+devdgehog@users.noreply.github.com>
2020-10-18 22:43:48 +0200

    challenge setup

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,4 +1,19 @@
 <template>
   <div>
+    Score
+    Rules
+
+    You Picked
+    The House Picked
+
+    You Win
+    You Lose
+
+    Play Again
+
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
   </div>
 </template>
