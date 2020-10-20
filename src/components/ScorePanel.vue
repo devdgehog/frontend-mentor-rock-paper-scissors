0bab9dbd564bc4a9c5b5bbf9f6b3c25117b2ae79
devdgehog <72769867+devdgehog@users.noreply.github.com>
2020-10-20 22:37:01 +0200

    component to display the score

---
+++ src/components/ScorePanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="score">
+    Score: {{ score }}
+  </div>
+</template>
+
+<script lang="ts">
+import { scoreStore } from '@/store';
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class ScorePanel extends Vue {
+  get score(): number {
+    return scoreStore.score;
+  }
+}
+</script>
