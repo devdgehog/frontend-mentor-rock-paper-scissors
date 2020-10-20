973b3ee808098f8bced9d444d3f2c5973e811cd9
devdgehog <72769867+devdgehog@users.noreply.github.com>
2020-10-20 22:36:46 +0200

    composent to represent a panel for the result of a round

---
+++ src/components/ResultPanel.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="results">
+    <div>
+      You Picked
+      <img :src="getUserIcon" />
+    </div>
+    <div>
+      The House Picked
+      <img :src="getHouseIcon" />
+    </div>
+    <button @pointerup.stop="playAgain()">Play again</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Choice, CHOICES } from '@/models/choice';
+import { choiceStore, scoreStore } from '@/store';
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class ResultPanel extends Vue {
+  get choices(): Choice[] {
+    return CHOICES;
+  }
+
+  storeUserChoiceAndPlay(choice: Choice) {
+    const houseChoiceIndex = Math.floor(Math.random() * CHOICES.length);
+    choiceStore.setUserChoice(choice);
+    choiceStore.setHouseChoice(CHOICES[houseChoiceIndex]);
+  }
+
+  get getUserIcon() {
+    return choiceStore.getUserChoice?.icon;
+  }
+
+  get getHouseIcon() {
+    return choiceStore.getHouseChoice?.icon;
+  }
+
+  updateScore() {
+    scoreStore.changeScore(choiceStore.result);
+  }
+
+  playAgain() {
+    this.updateScore();
+    choiceStore.resetChoices();
+  }
+}
+</script>
