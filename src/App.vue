aba48e4565c65a31ee6f07782dbcdaa93883e152
devdgehog <72769867+devdgehog@users.noreply.github.com>
2020-10-21 23:53:24 +0200

    set font and surrounding style

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,22 @@
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body,
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+
+#app {
+  background-image: linear-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+  color: white;
+  font-family: 'Barlow Semi Condensed', sans-serif;
+}
+</style>
