fd715edbe0e9f0e98744e920369be4351144aec6
devdgehog <72769867+devdgehog@users.noreply.github.com>
2020-11-01 00:08:40 +0100

    disable user selection

--- src/App.vue
+++ src/App.vue
@@ -36,5 +36,6 @@ body,
   background-image: linear-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
   color: #eee;
   font-family: 'Barlow Semi Condensed', sans-serif;
+  user-select: none;
 }
 </style>
